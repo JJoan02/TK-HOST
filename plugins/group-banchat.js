a487c467913196d3ea5d78c8502b62e21f2ffe34
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-12-22 17:53:29 -0500

    Update group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,11 +1,18 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
+    // Verifica si el usuario es admin, el propietario o el número del bot
+    if (!(isAdmin || isROwner || m.sender === conn.user.jid)) {
+        return conn.reply(m.chat, '🚫 No tienes permisos para usar este comando.', m)
+    }
+
+    // Banea el chat si cumple los requisitos
     global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `🚩 Chat Baneado con exito.`, m, rcanal)
+    await conn.reply(m.chat, `🚩 Chat baneado con éxito.`, m, rcanal)
     await m.react('✅')
 }
-handler.help = ['banearbot']
+
+// Lista de ayuda, etiquetas y comandos disponibles
+handler.help = ['banearbot', 'banchat']
 handler.tags = ['group']
-handler.command = ['banearbot', 'banchat']
-handler.group = true 
+handler.command = ['banearbot', 'banchat'] // Los comandos activadores
+handler.group = true // Solo se puede usar en grupos
 export default handler
