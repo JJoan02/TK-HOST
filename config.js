983fc3783d65a19ed576f80abe293a1e01384ec5
Niño Piña <ninopina10@gmail.com>
2024-12-22 16:22:25 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -71,6 +71,7 @@ global.instagram = 'https://www.instagram.com/joan_tk02'
 global.whatsApp = 'https://wa.me/51910234457'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363314192605628@newsletter", serverMessageId: 100, newsletterName: 'Tk-Host Channel', }, externalAdReply: { showAdAttribution: true, title: packname, body: 'JohanTk-Bot', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
 
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝗧𝗞-𝗛𝗢𝗦𝗧', orderTitle: 'packname', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
