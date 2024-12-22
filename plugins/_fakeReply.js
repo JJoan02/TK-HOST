5be941dde02dff387d9b98398b91c3964e5bc8a1
Niño Piña <ninopina10@gmail.com>
2024-12-22 16:48:56 -0600

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -11,8 +11,8 @@ let icono = imagenes[Math.floor(Math.random() * imagenes.length)]
 
 //Enlaces
 var canal =   'https://whatsapp.com/channel/0029Vakpp8h1Hsq5Y5kbF33t'
-var git = 'https://github.com/WillZek'  
-var github = 'https://github.com/WillZek/CrowBot-ST' 
+var git = 'https://github.com/JJoan02'  
+var github = 'https://github.com/JJoan02/TK-HOST' 
 var facebook = 'https://www.facebook.com/profile.php?id=100055985748040' 
 let tiktok = 'https://tiktok.com/@the_crow_legendario_bs'
 let correo = 'sm.ninopina10@gmail.com'
